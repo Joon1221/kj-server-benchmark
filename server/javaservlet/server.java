23180b38229a7404158d303bb6d8fbdef4bd1510
Joon1221 <52806289+Joon1221@users.noreply.github.com>
2020-08-21 14:46:31 -0700

    Add files via upload

--- server/javaservlet/server.java
+++ server/javaservlet/server.java
@@ -10,29 +10,43 @@ public class server extends HttpServlet {
     
     private String message;
     
+    
+    //--------------------------------------------------------------------------
+    // Intialize Server
+    //--------------------------------------------------------------------------
     public void init() throws ServletException {
         // Do required initialization
         message = "Hello World";
     }
     
+    //--------------------------------------------------------------------------
+    // Handle Get Request
+    //--------------------------------------------------------------------------
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         
         // Set response content type
         response.setContentType("text/html");
         
-        // Actual logic goes here.
+        // Output message
         PrintWriter out = response.getWriter();
         out.println("<h1>" + message + "</h1>");
     }
     
+    //--------------------------------------------------------------------------
+    // Handle Post Request
+    //--------------------------------------------------------------------------
     public void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
     
         // Set response content type
         response.setContentType("text/html");
         
-        // Read from request
+        //----------------------------------------------------------------------
+        // Read Data from Client
+        //----------------------------------------------------------------------
+
+        // read and store message from client
         StringBuilder buffer = new StringBuilder();
         BufferedReader reader = request.getReader();
         String line;
@@ -41,6 +55,7 @@ public class server extends HttpServlet {
         }
         String data = buffer.toString();
 
+        // parse the json message
         JsonParser parser = new JsonParser();
         JsonElement jsonElement = parser.parse(data);
         JsonObject rootObject = jsonElement.getAsJsonObject();
@@ -48,38 +63,50 @@ public class server extends HttpServlet {
         
         PrintWriter out = response.getWriter();
 
-        if (message.equals("0")) { // Video Request
-//            struct videoStreamingPacket {
-//                // custom made protocol
-//                uint8_t       type;
-//                uint8_t       body_length[3]; /* lenght of the body */
-//                uint8_t       stream_id[3]; /* reserved, must be "\0\0\0" */
-//                uint8_t       body[192]; /* data body */
-//            };
+        //----------------------------------------------------------------------
+        // Process Data
+        //----------------------------------------------------------------------
 
+        // process data according to request type
+        if (message.equals("0")) { // Video Request
+            //------------------------------------------------------------------
+            // videoStreamingPacket
+            //------------------------------------------------------------------
+            // custom made protocol
+            // uint8_t       type;
+            // uint8_t       body_length[3]; /* lenght of the body */
+            // uint8_t       stream_id[3]; /* reserved, must be "\0\0\0" */
+            // uint8_t       body[192]; /* data body */
+            //------------------------------------------------------------------
+            
+            // Open sample video file
             byte []chatMessage = new byte[192];
             InputStream is = new FileInputStream("../shared/1280x720.mp4");
             
             int readBytes = 0;
             
+            // Main loop
             while ((readBytes  = is.read(chatMessage)) != -1) {
+                // Send data as json string
                 String json = "{\"header\":0, \"bodyLength\":" + readBytes + ", \"chatId\":0, \"body\":\"" + new String(chatMessage) + "\"}";
                 out.println(json);
             }
             is.close();
         }
         else if (message.equals("1")) { // Chat Request
-//            struct chatPacket {
-//                // custom made protocol based off websocket
-//                uint8_t       header;
-//                uint8_t       body_length[3]; /* lenght of the body */
-//                uint8_t       chat_id[3]; /* reserved, must be "\0\0\0" */
-//                uint8_t       body[192]; /* data body */
-//            };
+            //------------------------------------------------------------------
+            // chatPacket
+            //------------------------------------------------------------------
+            // custom made protocol based off websocket
+            // uint8_t       header;
+            // uint8_t       body_length[3]; /* lenght of the body */
+            // uint8_t       chat_id[3]; /* reserved, must be "\0\0\0" */
+            // uint8_t       body[192]; /* data body */
+            //------------------------------------------------------------------
 
-            Random rand = new Random();
-            int value = rand.nextInt(192);
+
             
+            // Open sample text file
             byte []chatMessage = new byte[value];
             InputStream is = new FileInputStream("../shared/chat_message.txt");
             
@@ -88,6 +115,10 @@ public class server extends HttpServlet {
             is.read(chatMessage);
             is.close();
             
+            Random rand = new Random();
+            int value = rand.nextInt(192);
+            
+            // Send data as json string
             String json = "{\"header\":0, \"bodyLength\":" + value + ", \"chatId\":0, \"body\":\"" + new String(chatMessage) + "\"}";
 
             out.println(json);
